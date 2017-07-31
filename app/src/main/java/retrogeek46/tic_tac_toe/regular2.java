78359562951f0b704dc2e0233f8b669d29b9d0ce
samridhi vaid <samridhivaid@gmail.com>
2017-07-31 15:36:09 +0530

    added regular2

---
+++ app/src/main/java/retrogeek46/tic_tac_toe/regular2.java
@@ -0,0 +1,17 @@
+package retrogeek46.tic_tac_toe;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by samridhi on 31-07-2017.
+ */
+
+public class regular2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regular);
+    }
+}
