16bc0cf0845b8edb9f1cb77d8d73a71e98218288
samridhi vaid <samridhivaid@gmail.com>
2017-07-31 15:34:02 +0530

    new twoP class added

--- app/src/main/java/retrogeek46/tic_tac_toe/MainActivity.java
+++ app/src/main/java/retrogeek46/tic_tac_toe/MainActivity.java
@@ -5,26 +5,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = (Button) findViewById(R.id.button);
+        TextView oneP = (TextView) findViewById(R.id.oneP);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        assert oneP != null;
+        oneP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                launchActivity();
+                Intent intent = new Intent(MainActivity.this , regular.class);
+                startActivity(intent);
             }
         });
+
+        TextView twoP = (TextView) findViewById(R.id.twoP);
+
+        assert twoP != null;
+        twoP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent regular2 = new Intent(MainActivity.this, regular2.class);
+                startActivity(regular2);
+            }
+        });
+
+
     }
 
-    private void launchActivity() {
-        Intent intent = new Intent(this,regular.class);
-        startActivity(intent);
-    }
+
 }
