2f74bcad1f870e321019c3b711381c1ef086fe64
samridhi vaid <samridhivaid@gmail.com>
2017-07-31 22:57:54 +0530

    changes made to regular2

--- app/src/main/java/retrogeek46/tic_tac_toe/regular2.java
+++ app/src/main/java/retrogeek46/tic_tac_toe/regular2.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,6 +20,7 @@ public class regular2 extends AppCompatActivity {
 
     TextView t1,t2,t3,t4,t5,t6,t7,t8,t9;
     int cnt[]=new int[9];
+    String a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,72 +37,86 @@ public class regular2 extends AppCompatActivity {
         t8= (TextView) findViewById(R.id.tv8);
         t9= (TextView) findViewById(R.id.tv9);
 
+        Button player1=(Button) findViewById(R.id.player1);
+        Button player2=(Button) findViewById(R.id.player2);
+        player1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a="X";
+
+
+            }
+        });
+        player2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a="O";
+            }
+        });
         t1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (cnt[0]==1 || cnt[0]==0);
-                else {t1.setText("X");cnt[0]=1;}
+                t1.setText(a);
+                cnt[0]=1;
             }
         });
-
         t2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (cnt[1]==1 || cnt[1]==0);
-                else {t2.setText("X");cnt[1]=1;}
+                t2.setText(a);
+                cnt[1]=1;
             }
         });
         t3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (cnt[2]==1 || cnt[2]==0);
-                else {t3.setText("X");cnt[2]=1;}
+                t3.setText(a);
+                cnt[2]=1;
             }
         });
         t4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (cnt[3]==1 || cnt[3]==0);
-                else {t4.setText("X");cnt[3]=1;}
+                t4.setText(a);
+                cnt[3]=1;
             }
         });
         t5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (cnt[4]==1 || cnt[4]==0);
-                else {t5.setText("X");cnt[4]=1;}
+                t5.setText(a);
+                cnt[4]=1;
             }
         });
-
         t6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (cnt[5]==1 || cnt[5]==0);
-                else {t6.setText("X");cnt[5]=1;}
+                t6.setText(a);
+                cnt[5]=1;
             }
         });
         t7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (cnt[6]==1 || cnt[6]==0);
-                else {t7.setText("X");cnt[6]=1;}
+                t7.setText(a);
+                cnt[6]=1;
             }
         });
         t8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (cnt[7]==1 || cnt[7]==0);
-                else {t8.setText("X");cnt[7]=1;}
+                t8.setText(a);
+                cnt[7]=1;
             }
         });
         t9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (cnt[8]==1 || cnt[8]==0);
-                else {t9.setText("X");cnt[8]=1;}
+
+                t9.setText(a);
+                cnt[8]=1;
             }
         });
-
     }
 
     private void launchActivitymain() {
